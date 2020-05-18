a87c3a5d12783f872af853bdb130ed0d5b3e9141
moma <angelmomaa@gmail.com>
2020-05-18 13:14:30 -0700

    playing around with each method

--- enumerables.rb
+++ enumerables.rb
@@ -1,1 +1,21 @@
-# methods
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun', 'Jean', 'Pedro', 'Keisuke', 'Tláloc']
+
+# playing around with each method
+def my_each do
+  yield
+  p
+end
+
+puts friends.each { |friend| puts friend }
+
+puts friends.myeach { |friend| puts friend }
+
+
+friends.each_with_index {  }
+friends.select { |x| puts x != "Leo"}
+friends.all? { |x| x.length > 3}
+friends.any? 
+friends.none?
+friends.count
+friends.map
+friends.inject
