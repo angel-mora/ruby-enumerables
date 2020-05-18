da94a021665714286cd1142a81775a7e9a47561d
moma <angelmomaa@gmail.com>
2020-05-18 13:18:06 -0700

    added first method with rubocop observations addressed. However, Prefer each over for comment not addressed as replicating each method

--- enumerables.rb
+++ enumerables.rb
@@ -1,21 +1,7 @@
-friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun', 'Jean', 'Pedro', 'Keisuke', 'Tláloc']
-
-# playing around with each method
-def my_each do
-  yield
-  p
+module Enumerable
+  def my_each
+    for x in self do
+      yield(x)
+    end
+  end
 end
-
-puts friends.each { |friend| puts friend }
-
-puts friends.myeach { |friend| puts friend }
-
-
-friends.each_with_index {  }
-friends.select { |x| puts x != "Leo"}
-friends.all? { |x| x.length > 3}
-friends.any? 
-friends.none?
-friends.count
-friends.map
-friends.inject
