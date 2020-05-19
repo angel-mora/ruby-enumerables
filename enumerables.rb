6cbc340e3c5fad2f24bafbb031b16e0a03bc37f1
moma <angelmomaa@gmail.com>
2020-05-19 15:28:04 -0700

    debugging

--- enumerables.rb
+++ enumerables.rb
@@ -77,15 +77,15 @@ end
 friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun', 'Jean', 'Pedro', 'Keisuke', 'Tláloc']
 numbers = [5, 9, 2, 3, 415, 6, 98]
 
-# friends.my_each { |x| puts x }
-# friends.my_each_with_index { |x, i| puts " #{x}, #{i}" }
-# friends.my_select { |x| x != "Leo" }
-# friends.my_all? { |x| x.length >= 3 }
-# friends.my_any? { |x| x.length s>= 3 }
-# friends.my_none? { |x| x.length >= 3 }
-# friends.my_count { |x| x.length == 5 }
-# friends.my_map { |x| x.reverse }
-# friends.my_inject(:+)
+friends.my_each { |x| puts x }
+friends.my_each_with_index { |x, i| puts " #{x}, #{i}" }
+friends.my_select { |x| x != "Leo" }
+friends.my_all? { |x| x.length >= 3 }
+friends.my_any? { |x| x.length s>= 3 }
+friends.my_none? { |x| x.length >= 3 }
+friends.my_count { |x| x.length == 5 }
+friends.my_map { |x| x.reverse }
+numbers.my_inject(:+)
 
 
 
