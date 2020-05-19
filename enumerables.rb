18d16cb53112e2044cd9405659a220063e1c56c6
moma <angelmomaa@gmail.com>
2020-05-19 15:11:14 -0700

    added a couple more methods

--- enumerables.rb
+++ enumerables.rb
@@ -21,6 +21,33 @@ module Enumerable
     puts selected
   end
 
+  def my_all?
+    for x in self do
+      unless yield(x)
+        return false
+      end  
+    end
+    return true
+  end 
+  
+  def my_any?
+    for x in self do
+      if yield(x)
+        return true
+      end  
+    end
+    return false
+  end 
+
+  def my_none?
+    for x in self do
+      unless yield(x)
+        return true
+      end  
+    end
+    return false
+  end
+
 end
 
 #tests
@@ -29,5 +56,8 @@ numbers = [5, 9, 2, 3, 415, 6, 98]
 
 # friends.my_each { |x| puts x }
 # friends.my_each_with_index { |x, i| puts " #{x}, #{i}" }
-friends.my_select { |x| x != "Leo" }
+# friends.my_select { |x| x != "Leo" }
+# friends.my_all? { |x| x.length >= 3 }
+# friends.my_any? { |x| x.length >= 3 }
+# friends.my_none? { |x| x.length >= 3 }
 
