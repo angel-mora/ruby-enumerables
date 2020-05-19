cbd4e594f74851b2efe9bbfeb43a424982f37dfb
moma <angelmomaa@gmail.com>
2020-05-19 12:57:25 -0700

    working on simple version of methods, added my_select

--- enumerables.rb
+++ enumerables.rb
@@ -1,7 +1,33 @@
 module Enumerable
   def my_each
-    for x in self do
-      yield(x)
+    x = 0
+    while x < length
+      yield(self[x])
+      x += 1
     end
   end
+
+  def my_each_with_index
+    i = 0
+    while i < length
+      yield(self[i], i)
+      i += 1
+    end
+  end
+
+  def my_select
+    selected = []
+    self.my_each { |x| selected << x if yield(x) }
+    puts selected
+  end
+
 end
+
+#tests
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun', 'Jean', 'Pedro', 'Keisuke', 'Tláloc']
+numbers = [5, 9, 2, 3, 415, 6, 98]
+
+# friends.my_each { |x| puts x }
+# friends.my_each_with_index { |x, i| puts " #{x}, #{i}" }
+friends.my_select { |x| x != "Leo" }
+
