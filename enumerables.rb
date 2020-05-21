13a5bdbc700a991666dfe5172e880cd08528ebdc
moma <angelmomaa@gmail.com>
2020-05-20 23:01:58 -0700

    separated tests and enumerables, fixed most styling linters, about to add not considered cases

--- enumerables.rb
+++ enumerables.rb
@@ -25,10 +25,10 @@ module Enumerable
     for x in self do
       unless yield(x)
         return false
-      end  
+      end
     end
-    return true
-  end 
+    true
+  end
   
   def my_any?
     for x in self do
@@ -36,7 +36,7 @@ module Enumerable
         return true
       end  
     end
-    return false
+    false
   end 
 
   def my_none?
@@ -45,48 +45,46 @@ module Enumerable
         return true
       end  
     end
-    return false
+    false
   end
 
   def my_count
     counter = 0
     if block_given?
       for x in self do
-          counter += 1 if yield(x)
+        counter += 1 if yield(x)
       end
-    else  
-      counter = self.length
+    else
+      counter = length
     end
-    return counter
-  end      
+    counter
+  end
 
   def my_map
     new_array = []
-    self.my_each { |x| new_array.push(yield(x))}
-    return new_array
+    my_each { |x| new_array.push(yield(x)) }
+    new_array
   end
 
-  def my_inject (argument = nil)
-      accumulator = nil
-      my_each { |item| accumulator = accumulator ? accumulator.send(argument, item) : item }
-      return accumulator  
-    end  
+  def my_inject(argument = nil)
+    accumulator = nil
+    my_each { |item| accumulator = accumulator ? accumulator.send(argument, item) : item }
+    accumulator
+  end
+  # Multiply_els pending
+
 end
 
-#tests
-friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun', 'Jean', 'Pedro', 'Keisuke', 'Tláloc']
+# Tests, need to load them in another file
+friends = %w[Sharon Leo Leila Brian Arun Jean Pedro Keisuke Tláloc]
 numbers = [5, 9, 2, 3, 415, 6, 98]
 
 friends.my_each { |x| puts x }
-friends.my_each_with_index { |x, i| puts " #{x}, #{i}" }
-friends.my_select { |x| x != "Leo" }
+friends.my_each_with_index { |x, i| puts "#{x}, #{i}" }
+friends.my_select { |x| x != 'Leo' }
 friends.my_all? { |x| x.length >= 3 }
-friends.my_any? { |x| x.length s>= 3 }
+friends.my_any? { |x| x.length >= 3 }
 friends.my_none? { |x| x.length >= 3 }
 friends.my_count { |x| x.length == 5 }
 friends.my_map { |x| x.reverse }
 numbers.my_inject(:+)
-
-
-
-
---
+++ tests.rb
@@ -0,0 +1,15 @@
+require "enumerables.rb"
+
+# Tests, need to load them in another file
+friends = %w[Sharon Leo Leila Brian Arun Jean Pedro Keisuke Tláloc]
+numbers = [5, 9, 2, 3, 415, 6, 98]
+
+friends.my_each { |x| puts x }
+friends.my_each_with_index { |x, i| puts "#{x}, #{i}" }
+friends.my_select { |x| x != 'Leo' }
+friends.my_all? { |x| x.length >= 3 }
+friends.my_any? { |x| x.length >= 3 }
+friends.my_none? { |x| x.length >= 3 }
+friends.my_count { |x| x.length == 5 }
+friends.my_map { |x| x.reverse }
+numbers.my_inject(:+)
