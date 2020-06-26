4cd53f25e8d40e8f2940d0a3b3d769f38c0baf16
moma <angelmomaa@gmail.com>
2020-06-26 15:12:37 -0500

    add rspec init and enumerables_tests.rb

---
+++ spec/enumerables_tests.rb
@@ -0,0 +1,12 @@
+# spec/enumerables_tests.rb
+require './enumerables.rb'
+
+describe Enumerable do
+  include Enumerable
+  let(:friends) { %w[Sharon Leo Leila Brian Arun Jean Pedro Keisuke Tláloc] }
+    describe '#my_each' do
+      it 'method returns all elements inside an array' do
+        expect(friends.my_each { |x| puts x } ).to eql(["Sharon", "Leo", "Leila", "Brian", "Arun", "Jean", "Pedro", "Keisuke", "Tláloc"])
+      end
+    end
+end
