4a0362d8f5c8e0047c7ae9901b0b14e641b0143e
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-06-29 14:26:39 -0500

    testing the my_each method

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -2,10 +2,21 @@
 
 require_relative '../enumerables.rb'
 
-describe Enumerable do 
+describe Enumerable do
+  let(:friends) {%w[Sharon Leo Leila Brian Arun Jean Pedro Keisuke Tláloc]}
+  let(:friends_reslt) {%w[sharon leo leila brian arun jean pedro keisuke tláloc]}
   describe '#my_each' do 
-    it 'returns arrays elements' do
-      expect(true).to eql(false)
+    context 'when no block is given it returns Enumerable' do
+      it '#my_each' do
+        expect(friends.my_each).to be_an(friends.to_enum.class)
+      end
+    end
+    context 'when a block is given ' do
+      it '#my_each' do
+        output = []
+        friends.my_each {|elem| output << elem.downcase}
+        expect(output).to eql(friends_reslt)
+      end
     end
   end
 end
