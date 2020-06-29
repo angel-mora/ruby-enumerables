a4d387522dafa51780d3ac2e0cc1e72a5ef7b44a
moma <angelmomaa@gmail.com>
2020-06-29 14:59:45 -0500

    added my_each_with_index tests

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -5,13 +5,14 @@ require_relative '../enumerables.rb'
 describe Enumerable do
   let(:friends) {%w[Sharon Leo Leila Brian Arun Jean Pedro Keisuke Tláloc]}
   let(:friends_reslt) {%w[sharon leo leila brian arun jean pedro keisuke tláloc]}
+  
   describe '#my_each' do 
     context 'when no block is given it returns Enumerable' do
       it '#my_each' do
         expect(friends.my_each).to be_an(friends.to_enum.class)
       end
     end
-    context 'when a block is given ' do
+    context 'when a block is given' do
       it '#my_each' do
         output = []
         friends.my_each {|elem| output << elem.downcase}
@@ -19,4 +20,26 @@ describe Enumerable do
       end
     end
   end
+
+  let(:friends2) { %w[Sharon Leo Leila Brian Arun Jean] }
+  let(:friends_with_index) { ['Sharon 0', 'Leo 1', 'Leila 2', 'Brian 3', 'Arun 4', 'Jean 5'] }
+  describe 'my_each_with_index' do
+    context 'when no block is given it returns Enumerable' do
+      it '#my_each_with_index' do
+        expect(friends2.my_each_with_index).to be_an(friends2.to_enum.class)
+      end
+    end
+    context 'when a block is given' do
+      it '#my_each_with_index' do
+        output_with_index = []
+        friends2.my_each_with_index { |x, i| output_with_index << "#{x} #{i}"}
+        expect(output_with_index).to eql(friends_with_index)
+      end
+    end
+
+
+  end
 end
+
+
+
